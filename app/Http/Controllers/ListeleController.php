813578b6422784519ab6dfcfcce8ea0ba39fc6c2
huriyeseberoglu <huriye_seberoglu1907@hotmail.com>
2017-09-19 11:15:50 +0300

    array

---
+++ app/Http/Controllers/ListeleController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListeleController extends Controller
+{
+        function  dizi()  {
+        $iller = array(
+            "ADANA"          => "ADANA",
+            "ADIYAMAN"       => "ADIYAMAN",
+            "AFYONKARAHİSAR" => "AFYONKARAHİSAR",
+            "AĞRI"           => "AĞRI",
+            "AMASYA"         => "AMASYA",
+            "ANKARA"         => "ANKARA",
+            "ANTALYA"        => "ANTALYA",
+            "ARTVİN"         => "ARTVİN",
+            "AYDIN"          => "AYDIN",
+            "BALIKESİR"      => "BALIKESİR",
+            "BİLECİK"        => "BİLECİK",
+            "BİNGÖL"         => "BİNGÖL",
+            "BİTLİS"         => "BİTLİS",
+            "BOLU"           => "BOLU",
+            "BURDUR"         => "BURDUR",
+            "BURSA"          => "BURSA",
+            "ÇANAKKALE"      => "ÇANAKKALE",
+            "ÇANKIRI"        => "ÇANKIRI",
+            "ÇORUM"          => "ÇORUM",
+            "DENİZLİ"        => "DENİZLİ",
+            "DİYARBAKIR"     => "DİYARBAKIR",
+            "EDİRNE"         => "EDİRNE",
+            "ELAZIĞ"         => "ELAZIĞ",
+            "ERZİNCAN"       => "ERZİNCAN",
+            "ERZURUM"        => "ERZURUM",
+            "ESKİŞEHİR"      => "ESKİŞEHİR",
+            "GAZİANTEP"      => "GAZİANTEP",
+            "GİRESUN"        => "GİRESUN",
+            "GÜMÜŞHANE"      => "GÜMÜŞHANE",
+            "HAKKARİ"        => "HAKKARİ",
+            "HATAY"          => "HATAY",
+            "ISPARTA"        => "ISPARTA",
+            "MERSİN"         => "MERSİN",
+            "İSTANBUL"       => "İSTANBUL",
+            "İZMİR"          => "İZMİR",
+            "KARS"           => "KARS",
+            "KASTAMONU"      => "KASTAMONU",
+            "KAYSERİ"        => "KAYSERİ",
+            "KIRKLARELİ"     => "KIRKLARELİ",
+            "KIRŞEHİR"       => "KIRŞEHİR",
+            "KOCAELİ"        => "KOCAELİ",
+            "KONYA"          => "KONYA",
+            "KÜTAHYA"        => "KÜTAHYA",
+            "MALATYA"        => "MALATYA",
+            "MANİSA"         => "MANİSA",
+            "KAHRAMANMARAŞ"  => "KAHRAMANMARAŞ",
+            "MARDİN"         => "MARDİN",
+            "MUĞLA"          => "MUĞLA",
+            "MUŞ"            => "MUŞ",
+            "NEVŞEHİR"       => "NEVŞEHİR",
+            "NİĞDE"          => "NİĞDE",
+            "ORDU"           => "ORDU",
+            "RİZE"           => "RİZE",
+            "SAKARYA"        => "SAKARYA",
+            "SAMSUN"         => "SAMSUN",
+            "SİİRT"          => "SİİRT",
+            "SİNOP"          => "SİNOP",
+            "SİVAS"          => "SİVAS",
+            "TEKİRDAĞ"       => "TEKİRDAĞ",
+            "TOKAT"          => "TOKAT",
+            "TRABZON"        => "TRABZON",
+            "TUNCELİ"        => "TUNCELİ",
+            "ŞANLIURFA"      => "ŞANLIURFA",
+            "UŞAK"           => "UŞAK",
+            "VAN"            => "VAN",
+            "YOZGAT"         => "YOZGAT",
+            "ZONGULDAK"      => "ZONGULDAK",
+            "AKSARAY"        => "AKSARAY",
+            "BAYBURT"        => "BAYBURT",
+            "KARAMAN"        => "KARAMAN",
+            "KIRIKKALE"      => "KIRIKKALE",
+            "BATMAN"         => "BATMAN",
+            "ŞIRNAK"         => "ŞIRNAK",
+            "BARTIN"         => "BARTIN",
+            "ARDAHAN"        => "ARDAHAN",
+            "IĞDIR"          => "IĞDIR",
+            "YALOVA"         => "YALOVA",
+            "KARABÜK"        => "KARABÜK",
+            "KİLİS"          => "KİLİS",
+            "OSMANİYE"       => "OSMANİYE",
+            "DÜZCE"          => "DÜZCE"
+        );
+        foreach($iller as $deger)
+
+        {
+            echo $deger."<br/>";
+        }
+
+
+    }  
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,2 @@
-
+<title>İL LİSTESİ</title>
 <P style="position: absolute;right: 600px;top: 5px;font-size: 50px;">İL'LER</P>
